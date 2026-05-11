1ab93816b35eaea0afab3f421c24a60eadc56f68
YoavPaz <150516622+YoavPaz@users.noreply.github.com>
2026-05-11 16:42:02 +0300

    Refactor tree.py to add new AST classes and methods

--- tree.py
+++ tree.py
@@ -1,5 +1,4 @@
-from interpeter import Interpeter, Token, TYPE
-
+from interpeter import Interpeter, Token, TYPE, RESERVED_KEYWORDS
 
 class AST(object):
     pass
@@ -12,6 +11,7 @@ class BinOpt(object):
         self.right = right
     def __str__(self) -> str:
         return f'[op: {self.op}, left: {self.left}, right: {self.right}]'
+    __repr__ = __str__
 
 class UnaryOp(object):
     def __init__(self, op, expr) -> None:
@@ -20,6 +20,7 @@ class UnaryOp(object):
         self.expr = expr
     def __str__(self) -> str:
         return f'[{self.op.value}{self.expr}]'
+    __repr__ = __str__
         
 class Num(AST):
     def __init__(self, token: Token):
@@ -28,18 +29,131 @@ class Num(AST):
         self.type = token.t
     def __str__(self) -> str:
         return f'num: {self.value}'
+    __repr__ = __str__
+
+class Compound(AST):
+    def __init__(self) -> None:
+        self.children = []
+    def __str__(self) -> str:
+        return f'compound: {self.children}'
+    __repr__ = __str__
+
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+    def __str__(self) -> str:
+        return f'Assign: {self.left} {self.op} {self.right}'
+    __repr__ = __str__
+
+class Var(AST):
+    def __init__(self, token) -> None:
+        self.token = token
+        self.value = self.token.value
+    def __str__(self) -> str:
+        return f'var: {self.token} {self.value}'
+    __repr__ = __str__
+
+class NoOp(AST):
+    def __str__(self) -> str:
+        return f'NoOp'
+    __repr__ = __str__
 
 class Parser():
     def __init__(self, lexer: Interpeter):
         self.lexer = lexer
-
         self.current_token = self.lexer.next_token()
+
+    def program(self):
+        """program : compound_statement DOT"""
+        node = self.compound_statement()
+        self.eat(TYPE.DOT)
+        return node
+
+    def compound_statement(self):
+        """
+        compound_statement: BEGIN statement_list END
+        """
+        self.eat(TYPE.BEGIN)
+        nodes = self.statement_list()
+        self.eat(TYPE.END)
+
+        root = Compound()
+        for node in nodes:
+            root.children.append(node)
+
+        return root
+
+    def statement_list(self):
+        """
+        statement_list : statement
+                       | statement SEMI statement_list
+        """
+        node = self.statement()
+
+        results = [node]
+
+        while self.current_token.t == TYPE.SEMI:
+            self.eat(TYPE.SEMI)
+            results.append(self.statement())
+
+        if self.current_token.t == TYPE.ID:
+            self.error()
+
+        return results
+
+    def statement(self):
+        """
+        statement : compound_statement
+                  | assignment_statement
+                  | empty
+        """
+        if self.current_token.t == TYPE.BEGIN:
+            node = self.compound_statement()
+        elif self.current_token.t == TYPE.ID:
+            node = self.assignment_statement()
+        else:
+            node = self.empty()
+        return node
+
+    def assignment_statement(self):
+        """
+        assignment_statement : variable ASSIGN expr
+        """
+        left = self.variable()
+        token = self.current_token
+        self.eat(TYPE.ASSIGN)
+        right = self.expr()
+        node = Assign(left, token, right)
+        return node
+
+    def variable(self):
+        """
+        variable : ID
+        """
+        node = Var(self.current_token)
+        self.eat(TYPE.ID)
+        return node
+
+    def empty(self):
+        """An empty production"""
+        return NoOp()
 
     def eat(self, type: TYPE):
         if self.current_token.t == type:
             self.current_token = self.lexer.next_token()
         else:
             self.error()
+
+    def _id(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isalnum():
+            result += self.current_char
+            self.current_index += 1
+        
+        token = RESERVED_KEYWORDS.get(result)
+        return token
 
     def error(self):
         raise Exception('Invalid syntax!')
@@ -50,17 +164,20 @@ class Parser():
             self.eat(TYPE.PLUS)
             node = UnaryOp(token, self.factor())
             return node
-        if token.t == TYPE.MINUS:
+        elif token.t == TYPE.MINUS:
             self.eat(TYPE.MINUS)
             node = UnaryOp(token, self.factor())
             return node
-        if token.t == TYPE.INTEGER:
+        elif token.t == TYPE.INTEGER:
             self.eat(TYPE.INTEGER)
             return Num(token)
         elif token.t == TYPE.P_IN:
             self.eat(TYPE.P_IN)
             node = self.expr()
             self.eat(TYPE.P_OUT)
+            return node
+        else:
+            node = self.variable()
             return node
     def term(self):
         node = self.factor()
@@ -101,11 +218,24 @@ class Parser():
         return node
 
     def parse(self):
-        return self.expr()
+        node = self.program()
+        if self.current_token.t != TYPE.EOF:
+            self.error()
+        return node
 
 class NodeVisistor(object):
     def __init__(self):
         self.RPN = ""
+        self.call_stack = []
+        self.current_scope = -1
+    def make_scope(self):
+        self.call_stack.append({})
+    def remove_scope(self):
+        self.call_stack.pop()
+    def get_scope(self) -> dict:
+        if self.current_scope == None: # no scope...
+                self.make_scope()
+        return self.call_stack[self.current_scope]
     def visit(self, node):
         if node is None:
             raise Exception('Invalid Syntax')
@@ -120,6 +250,27 @@ class NodeVisistor(object):
             val: float = self.visit(node.expr)
             if node.token.value == '+': return +val
             if node.token.value == '-': return -val
+        if isinstance(node, Compound):
+            #print(f"making new compound, call stack: {self.call_stack}")
+            self.make_scope()
+            for child in node.children:
+                self.visit(child)
+            print(f"removing scope, full scope: {self.call_stack}")
+            self.remove_scope()
+            return None
+        if isinstance(node, Assign):
+            self.get_scope()[node.left.value] = self.visit(node.right)
+            #print(f"assigning var, call stack: {self.call_stack}")
+        if isinstance(node, Var):
+            var_name = node.value
+            val = self.get_scope().get(var_name)
+            #print(f"reading stack: {self.call_stack}")
+            if val is None:
+                raise NameError(repr(var_name))
+            else:
+                return val
+        if isinstance(node, NoOp):
+            return None        
 
         left = self.visit(node.left)
         right = self.visit(node.right)
@@ -129,12 +280,17 @@ class NodeVisistor(object):
         if node.token.value == '-': return left - right
         if node.token.value == '*': return left * right
         if node.token.value == '/': return left / right
-while True:
-    inp = input('calc> ')
-    if inp == 'q':
-        break
+
+def run_script(name: str):
+    with open(name, "r") as f:
+        inp = f.read()
+    inp = " ".join(inp.split()) + " "
     lexer = Interpeter(inp)
     parser = Parser(lexer)
-    nodeVisitor = NodeVisistor()
+    node_visitor = NodeVisistor()
+    node = parser.parse()
+    print(node)
+    node_visitor.visit(node)
+    print(node_visitor.call_stack)
 
-    print(nodeVisitor.visit(parser.parse()))
+run_script("test.end")
